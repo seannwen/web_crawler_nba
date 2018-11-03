edf8fe1277b4a36a23e16d6162fe7f2b96d435f2
tsunhsiang wen <wents@oregonstate.edu>
2018-11-02 17:17:26 -0700

    fixed bug

--- nba.py
+++ nba.py
@@ -12,24 +12,24 @@ def get_web_data(date):
     # get the score data from the nba website
     res = requests.get('https://data.nba.net/prod/v2/{}/scoreboard.json'.format(date))
     json_object = json.loads(res.text)
+    # print (json_object)
     # print json.dumps(json_object, indent=4)
     return json_object
 
 
-def team_name(team_name_abbr):
+def team_name():
     # get the NBA team names and Abbreviations
     res = requests.get('https://en.wikipedia.org/wiki/Wikipedia:WikiProject_National_Basketball_Association/National_Basketball_Association_team_abbreviations')
     bs4_html = BeautifulSoup(res.text, "html.parser")
     table = bs4_html.find('table', {'class': 'wikitable sortable'})
-
     team_dict = dict()
-    for team in table:
-        if isinstance(team, element.Tag) and team.find('a'):
-            team_abbr = team.find('td').text
-            team_full = team.find('a')['title']
+    table_with_label = table.find_all('tr')
+    for team in table_with_label:
+        if team.find('a'):
+            team_abbr = team.find('td').text.rstrip()
+            team_full = team.find('a').text
             team_dict[team_abbr] = team_full
-
-    return team_dict[team_name_abbr]
+    return team_dict
 
 
 def insert_sting_middle(str, word):
@@ -51,13 +51,14 @@ def get_daily_score(web):
         utc_time = datetime.strptime(start_time, utc_format)
         local_time = utc_time + timedelta(hours=-8)
         table = PrettyTable(['Team', 'W/L', 'Score'])
+        team_dict = team_name()
         table.add_row([
-            '({:>3}) {:^23}'.format(visitor['triCode'], team_name(visitor['triCode'])),
+            '({:>3}) {:^23}'.format(visitor['triCode'], team_dict[visitor['triCode']]),
             'W{}/L{}'.format(visitor['win'], visitor['loss']),
             'N/A' if not visitor['score'] else visitor['score']
         ])
         table.add_row([
-            '({:>3}) {:^23}'.format(host['triCode'], team_name(host['triCode'])),
+            '({:>3}) {:^23}'.format(host['triCode'], team_dict[host['triCode']]),
             'W{}/L{}'.format(host['win'], host['loss']),
             'N/A' if not host['score'] else host['score']
         ])
@@ -179,12 +180,11 @@ if __name__ == "__main__":
     date = datetime.strftime("%Y%m%d")
 
     # date = raw_input('Please enter the date(Ex: 20180101): \n')
-    # date = '20180305'
+    # date = '20170306'
 
     # team_abbr = raw_input("Please enter the team abbreviation(Ex: CLE): \n")
     # team = insert_sting_middle('''''', team_abbr.upper())
     team = 'LAL'
-
     web = get_web_data(date)  # get the data from website
     get_daily_score(web)
     # print '\n'.join(message)
